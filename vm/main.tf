90eef3c544cb429fb1cbf148941db467682f5242
Raja Charuvil <rajaasokan.charuvil@liebherr.com>
2026-01-09 14:58:58 +0100

    updated

--- vm/main.tf
+++ vm/main.tf
@@ -9,41 +9,45 @@ resource "azurerm_resource_group" "main" {
   tags = var.tags
 }
 
-module "testvm" {
-  source  = "Azure/avm-res-compute-virtualmachine/azurerm"
-  version = "0.16.0"
+resource "azurerm_network_interface" "main" {
+  name                = "${local.name}-nic1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
-  admin_username                     = "azureuser"
-  disable_password_authentication    = false
-  enable_telemetry                   = true
-  encryption_at_host_enabled         = false
-  generate_admin_password_or_ssh_key = true
-  location                           = azurerm_resource_group.main.location
-  name                               = local.name
-  resource_group_name                = azurerm_resource_group.main.name
-  os_type                            = "Linux"
-  sku_size                           = var.vm_sku_size
-  zone                               = var.vm_zone
-
-
-  network_interfaces = {
-    network_interface_1 = {
-      name = "${local.name}-nic1"
-      ip_configurations = {
-        ip_configuration_1 = {
-          name                          = "${local.name}-ipconfig1"
-          private_ip_subnet_resource_id = var.vm_subnet_id
-        }
-      }
-    }
+  ip_configuration {
+    name                          = "${local.name}-ipconfig1"
+    subnet_id                     = var.vm_subnet_id
+    private_ip_address_allocation = "Dynamic"
   }
 
-  os_disk = {
+  tags = var.tags
+}
+
+resource "random_password" "admin_password" {
+  length  = 16
+  special = true
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = local.name
+  location                        = azurerm_resource_group.main.location
+  resource_group_name             = azurerm_resource_group.main.name
+  size                            = var.vm_sku_size
+  admin_username                  = "azureuser"
+  admin_password                  = random_password.admin_password.result
+  disable_password_authentication = false
+  zone                            = var.vm_zone
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+
+  os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
 
-  source_image_reference = {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts-gen2"
@@ -51,5 +55,4 @@ module "testvm" {
   }
 
   tags = var.tags
-
 }
